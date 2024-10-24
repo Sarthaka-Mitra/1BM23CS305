496fc8384e034134ac062ad9b6e567bb5317e460
Sarthaka-Mitra <167693979+Sarthaka-Mitra@users.noreply.github.com>
2024-10-24 10:13:58 +0530

    Update QuadSol.java

--- QuadSol.java
+++ QuadSol.java
@@ -12,11 +12,11 @@ class QuadSol
         
         float r1,r2, dis;
 	dis = b * b - 4 * a * c;
-        if (discriminant < 0) 
+        if (dis < 0) 
             System.out.println("No real solution");
          else {
-            r1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            r2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            r1 = (-b + Math.sqrt(dis)) / (2 * a);
+            r2 = (-b - Math.sqrt(dis)) / (2 * a);
             System.out.println("Root 1: " + r1);
             System.out.println("Root 2: " + r2);
         }
