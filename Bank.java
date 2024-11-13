3f3dfda280f2b76f1ba3094eca716b1a536f3b5c
Sarthaka-Mitra <167693979+Sarthaka-Mitra@users.noreply.github.com>
2024-11-13 22:53:06 +0530

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
 class Account {
 	String acc_name;
-	ìnt  acc_no;
+	int  acc_no;
 	boolean if_curract, if_saving;
 	long balance;
 	
 	static	Scanner sm = new Scanner(System.in);
 
-	class Account(String name, int no, long bal) {
+	Account(String name, int no, long bal) {
 		acc_name = name;
 		acc_no = no;
 		balance = bal;
 	}
 
-	void check(){
+	/*void check(){
 		System.out.println("Are you holding a savings account?: ");
 		if_saving = sm.nextBoolean();
 		System.out.println("Are you holding a current account?: ");
 		if_curract = sm.nextBoolean();
-	}
+	}*/
 
 
 	void deposit(){
 		System.out.println("Enter amount to be added");
-		long new = sm.nextLong();
-		balance += new;
+		long nw = sm.nextLong();
+		balance += nw;
 	}
 
 	void disp(){
@@ -38,7 +38,7 @@ class Account {
 
 class Curr_Acct extends Account {
 	
-	class Curr_Act(String name, int no, long bal) {
+	Curr_Acct(String name, int no, long bal) {
 		super(name, no, bal);
 		service_charges();
 		if_curract = true;
@@ -73,16 +73,16 @@ class Curr_Acct extends Account {
 class Sav_Acct extends Account {
 	float cmp_int;
 	
-	class Save_Acct(String name, int no, long bal, float interest) {
+	Sav_Acct(String name, int no, long bal) { //float interest) {
 		super(name, no,bal);
-		cmp_int = interest;
-		balance *= (1+cmp_int);
-		if_savings = true;
+		//cmp_int = interest;
+		//balance *= (1+cmp_int);
+		if_saving = true;
 	}
 
 	void dep_int(){
 		int flag = 1;
-		while (flag) {
+		while (flag==1) {
 			System.out.println("Enter the compound interest (value between 0 and 1): ");
 			cmp_int = sm.nextFloat();
 
@@ -115,9 +115,44 @@ class Sav_Acct extends Account {
 }
 
 class Bank {
-	Scanner sc = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
 
 	//Creating a current account AND savings account
 	
-	
+	public static void main(String main[]) {
+		System.out.println("Enter your name:");
+		String name = sc.next();
 		
+		System.out.println("Enter your Bank Account number:");
+		int no = sc.nextInt();
+		
+		System.out.println("Enter the amount to be entered:");
+		long amt = sc.nextLong();
+		Curr_Acct a1 = new Curr_Acct(name, no, amt);
+
+		Sav_Acct a2 = new Sav_Acct(name, no, amt);
+		
+		a1.disp();
+		a1.deposit();
+	
+		a1.cheque_book();
+		a1.disp();
+	 
+
+		a1.cheque_book();
+		a1.disp();
+		//end of Current account activities
+
+		a2.disp();
+
+		a2.deposit();
+		a2.dep_int();
+		
+		a2.disp();
+		a2.withdraw();
+		
+		a2.disp();
+		//end of Savings account activities
+	}
+}
+
