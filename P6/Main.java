912db890c1af4c9f38528b69f1ddab9edebb2ac2
Sarthaka-Mitra <167693979+Sarthaka-Mitra@users.noreply.github.com>
2024-11-20 22:54:21 +0530

    Add files via upload

--- P6/Main.java
+++ P6/Main.java
@@ -1,21 +1,58 @@
 import CIE.*;
 import SEE.*;
+import java.util.Scanner;
+
 
 class Main {
 	public static void main(String xx[]){
-		short i_i1[] = {45, 34, 32, 50, 50};
-		short i_i2[] = {45, 41, 50, 50, 42};
-		CIE.Internals i1 = new CIE.Internals("ABC", "1BM23CS305", 3, i_i1, i_i2);
+		Scanner sm = new Scanner(System.in).useDelimiter("\n");
 
-		i1.display();
-		System.out.println();
+		System.out.println("Enter the number of students:");
+		int n = sm.nextInt();
 
-		short i_e[] = {89, 98, 90, 95, 95};
+		CIE.Internals Itls[] = new CIE.Internals[n];
+		SEE.Externals Exls[] = new SEE.Externals[n];
 
-		SEE.Externals i2 = new SEE.Externals("ABC", "1BM23CS305", 3, i_e);
+		for(int i = 0; i < n; i++){
+			System.out.println("Enter name:");
+			String name = sm.next();
 
-		i2.display(i1);
+			System.out.println("Enter USN:");
+			String USN = sm.next();
 
+
+			System.out.println("Enter marks for CIE 1:");
+
+			short cie1[] = new short[5];
+			short cie2[] = new short[5];
+
+
+			for(int j = 0; j<5;j++)
+				cie1[j] = sm.nextShort();
+
+			System.out.println("Enter marks for CIE 2:");
+			
+			for(int j = 0; j<5;j++)
+				cie2[j] = sm.nextShort();
+
+
+			System.out.println("Enter SEE Marks:");
+
+			short see[] = new short[5];
+
+			for(int j = 0; j<5;j++)
+				see[j] = sm.nextShort();
+			
+			Itls[i] = new CIE.Internals(name, USN, 3, cie1, cie2);
+			Itls[i].display();
+
+			System.out.println("");
+
+			Exls[i] = new SEE.Externals(name, USN, 3, see);
+			Exls[i].display(Itls[i]);
+
+			System.out.println("");
+		}
 	}
 
 }
